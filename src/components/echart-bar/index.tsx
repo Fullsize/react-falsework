b4210cf4baf85365e0109a8f7ed4943b809aa5e4
无涯 <1619686166@qq.com>
2023-09-11 17:24:54 +0800

    feat: 更新

---
+++ src/components/echart-bar/index.tsx
@@ -0,0 +1,149 @@
+import React, { useEffect, useState } from 'react';
+import Echart from '@/components/echart';
+import _ from 'lodash';
+interface Props {
+  data: any;
+  xDataName?: string;
+  data_deconstruction?: {
+    name: string;
+    unit: string;
+    value: string;
+    type: string;
+  };
+  isCustomBar?: boolean;
+}
+
+const EchartBar = (props: Props) => {
+  const {
+    xDataName = 'time_name',
+    data_deconstruction = {
+      name: 'index_code_full_cname',
+      value: 'val',
+      unit: 'unit_name',
+      type: 'type',
+    },
+  } = props;
+  const [option, setOption] = useState({});
+  useEffect(() => {
+    const datas = props.data;
+    const noData = datas?.empty || _.isEmpty(datas);
+    if (noData) {
+      setOption({});
+      return;
+    }
+    // x轴数据
+    const xData: any = _.uniqBy(datas, xDataName).map(
+      (item: any) => `${item[xDataName]}`,
+    );
+    // 数据分类
+    const group = _.uniqBy(datas, data_deconstruction.name);
+    // echart 类型
+    const types = _.uniqBy(datas, data_deconstruction.type).map((item, i) => ({
+      ...item,
+      yIndex: i,
+    }));
+    // y轴设置
+    const yAxis = types.map((item: any) => ({
+      type: 'value',
+      name: item.unit_name,
+      nameTextStyle: {
+        fontSize: 14,
+      },
+      scale: true,
+      alignTicks: true,
+      axisLine: {
+        show: true,
+        lineStyle: {
+          show: true,
+        },
+      },
+      splitLine: {
+        show: true,
+        lineStyle: {
+          show: true,
+          type: 'dashed',
+        },
+      },
+      axisLabel: {
+        fontSize: 14,
+      },
+    }));
+    //
+    const series = group.map((item: any) => {
+      const currentType = _.find(types, [data_deconstruction.type, item.type]);
+      const json: any = {
+        name: item[data_deconstruction.name],
+        type: item.type,
+        smooth: false,
+        step: false,
+        symbol: 'none',
+        yAxisIndex: currentType?.yIndex,
+        barMaxWidth: '30',
+        data: datas
+          .filter(
+            (ddd: any) =>
+              ddd[data_deconstruction.name] == item[data_deconstruction.name],
+          )
+          .map((a: any) => ({
+            lineStyle: {
+              width: 2,
+            },
+            symbolSize: xDataName == 'time_name' ? 4 : 10, // 调整symbol 大小，类目轴使用
+            name: a.index_code_full_cname,
+            value: ['' + a[xDataName], a[data_deconstruction.value]],
+            unit: a[data_deconstruction.unit],
+          })),
+      };
+      return json;
+    });
+    const option = {
+      xAxis: {
+        type: 'category',
+        data: xData,
+        splitArea: {
+          show: false,
+        },
+        splitLine: {
+          show: false,
+        },
+        axisLabel: {
+          fontSize: 14,
+        },
+        axisLine: {
+          lineStyle: {
+            show: true,
+          },
+        },
+      },
+      grid: {
+        containLabel: true,
+        left: 16,
+        right: 16,
+        top: 50,
+        bottom: 10,
+      },
+      yAxis,
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+          type: 'shadow',
+        },
+        formatter: function (params: any[] | null) {
+          let tip = '';
+          if (params !== null && params.length) {
+            params.map((item: any) => {
+              tip += `${item.marker}${item.seriesName}:  ${item.value[1]}${item.data?.unit}<br/>`;
+              return item;
+            });
+            return `${params[0].name}<br/>${tip}`;
+          }
+          return tip;
+        },
+      },
+      series,
+    };
+    setOption(option);
+  }, [props.data]);
+  return <Echart option={option} />;
+};
+export default EchartBar;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Echart from '@/components/echart';
+import EchartBar from '@/components/echart-bar';
 import styles from './index.module.css';
 // import bg from "@images/1.png";
 import './index.css';
@@ -7,7 +7,198 @@ const Page = () => {
   return (
     <div className={styles['constainer']}>
       <div style={{ width: 300, height: 180 }}>
-        <Echart option={{}} />
+        <EchartBar
+          data={[
+            {
+              time_name: '2020Q2',
+              val: 2661.15,
+
+              index_code_full_cname: '增长值',
+              unit_name: '亿元',
+
+              type: 'bar',
+            },
+            {
+              time_name: '2020Q2',
+              val: 0.7,
+
+              index_code_full_cname: '增涨速度',
+              unit_name: '%',
+
+              type: 'line',
+            },
+            {
+              time_name: '2020Q3',
+              val: 4143.97,
+              index_code_full_cname: '增长值',
+              unit_name: '亿元',
+              type: 'bar',
+            },
+            {
+              time_name: '2020Q3',
+              val: 1.4,
+              index_code_full_cname: '增涨速度',
+              unit_name: '%',
+              type: 'line',
+            },
+            {
+              time_name: '2020Q4',
+              val: 6000.66,
+              index_code_full_cname: '增长值',
+              unit_name: '亿元',
+              type: 'bar',
+            },
+            {
+              time_name: '2020Q4',
+              val: 3.3,
+              index_code_full_cname: '增涨速度',
+              unit_name: '%',
+              type: 'line',
+            },
+            {
+              time_name: '2021Q1',
+              val: 1434.49,
+              index_code_full_cname: '增长值',
+              unit_name: '亿元',
+              type: 'bar',
+            },
+            {
+              time_name: '2021Q1',
+              val: 17.4,
+              index_code_full_cname: '增涨速度',
+              unit_name: '%',
+              type: 'line',
+            },
+            {
+              time_name: '2021Q2',
+              val: 3188.61,
+              index_code_full_cname: '增长值',
+              unit_name: '亿元',
+              type: 'bar',
+            },
+            {
+              time_name: '2021Q2',
+              val: 13.7,
+              index_code_full_cname: '增涨速度',
+              unit_name: '%',
+              type: 'line',
+            },
+            {
+              time_name: '2021Q3',
+              val: 4900.95,
+              index_code_full_cname: '增长值',
+              unit_name: '亿元',
+              type: 'bar',
+            },
+            {
+              time_name: '2021Q3',
+              val: 11.4,
+              index_code_full_cname: '增涨速度',
+              unit_name: '%',
+              type: 'line',
+            },
+            {
+              time_name: '2021Q4',
+              val: 6795.26,
+
+              index_code_full_cname: '增长值',
+              unit_name: '亿元',
+
+              type: 'bar',
+            },
+            {
+              time_name: '2021Q4',
+              val: 8.7,
+              index_code_full_cname: '增涨速度',
+              unit_name: '%',
+              type: 'line',
+            },
+            {
+              time_name: '2022Q1',
+              val: 1609.64,
+              index_code_full_cname: '增长值',
+              unit_name: '亿元',
+              type: 'bar',
+            },
+            {
+              time_name: '2022Q1',
+              val: 5.3,
+              index_code_full_cname: '增涨速度',
+              unit_name: '%',
+              type: 'line',
+            },
+            {
+              time_name: '2022Q2',
+              val: 3360.14,
+              index_code_full_cname: '增长值',
+              unit_name: '亿元',
+              type: 'bar',
+            },
+            {
+              time_name: '2022Q2',
+              val: 3.2,
+              index_code_full_cname: '增涨速度',
+              unit_name: '%',
+              type: 'line',
+            },
+            {
+              time_name: '2022Q3',
+              val: 5236.13,
+              index_code_full_cname: '增长值',
+              unit_name: '亿元',
+              type: 'bar',
+            },
+            {
+              time_name: '2022Q3',
+              val: 4.3,
+              index_code_full_cname: '增涨速度',
+              unit_name: '%',
+              type: 'line',
+            },
+            {
+              time_name: '2022Q4',
+              val: 7350.55,
+              index_code_full_cname: '增长值',
+              unit_name: '亿元',
+              type: 'bar',
+            },
+            {
+              time_name: '2022Q4',
+              val: 4.4,
+              index_code_full_cname: '增涨速度',
+              unit_name: '%',
+              type: 'line',
+            },
+            {
+              time_name: '2023Q1',
+              val: 1738.76,
+              index_code_full_cname: '增长值',
+              unit_name: '亿元',
+              type: 'bar',
+            },
+            {
+              time_name: '2023Q1',
+              val: 6.1,
+              index_code_full_cname: '增涨速度',
+              unit_name: '%',
+              type: 'line',
+            },
+            {
+              time_name: '2023Q2',
+              val: 3621.93,
+              index_code_full_cname: '增长值',
+              unit_name: '亿元',
+              type: 'bar',
+            },
+            {
+              time_name: '2023Q2',
+              val: 7.9,
+              index_code_full_cname: '增涨速度',
+              unit_name: '%',
+              type: 'line',
+            },
+          ]}
+        />
       </div>
     </div>
   );
