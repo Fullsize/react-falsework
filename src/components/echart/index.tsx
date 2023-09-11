ffb3f2eb8d4f3f8325352c121fc05531321ae460
无涯 <1619686166@qq.com>
2023-09-11 16:48:53 +0800

    feat: 更新

--- src/components/echart/index.tsx
+++ src/components/echart/index.tsx
@@ -8,17 +8,29 @@ interface Props {
   className?: string;
   style?: React.CSSProperties;
   option: OPTIONTYPE;
-  getEchartObj?: (echart: any) => void;
 }
 const Echart = (props: Props, ref: React.Ref<unknown> | undefined) => {
-  const echartRef = useRef(null);
+  const echartRef = useRef<any>(null);
   // 获取实例
   const getInstance = () => echartRef.current;
-  const setOption = (chartObj: any) => {
-    if (!chartObj) {
-      return;
+
+  const init = () => {
+    // 判断ref是否存在
+    if (!echartRef.current) {
+      return false;
     }
-    chartObj.resize();
+    // 注册主题
+    echarts.registerTheme('echart_theme', white);
+    // 监测是否有echart示例,没有实例就创建
+    let chart = echarts.getInstanceByDom(echartRef.current);
+    if (!chart) {
+      chart = echarts.init(echartRef.current, 'echart_theme');
+    }
+    // setOption
+    setOption(chart);
+    return;
+  };
+  const setOption = (chartObj: any) => {
     // option 为空
     if (_.isEmpty(props.option)) {
       chartObj.setOption({
@@ -33,26 +45,24 @@ const Echart = (props: Props, ref: React.Ref<unknown> | undefined) => {
     console.log('echart option', _.isEmpty(props.option), props.option);
     chartObj.setOption(props.option, true);
   };
-  const init = () => {
-    // 判断ref是否存在
-    if (!echartRef.current) {
-      return false;
-    }
-    // 注册主题
-    echarts.registerTheme('echart_theme', white);
-    // 监测是否有echart示例
-    let chart = echarts.getInstanceByDom(echartRef.current);
-    if (!chart) {
-      chart = echarts.init(echartRef.current, 'echart_theme');
-    }
-    props.getEchartObj?.(chart);
-    setOption(chart);
-    return;
-  };
+  // 窗口自适应并开启过渡动画
+  // useEffect(() => {
+  //   window.addEventListener('resize', () => {
+  //     if (echartRef.current) {
+  //       echartRef.current.resize({
+  //         animation: { duration: 300 },
+  //       });
+  //     }
+  //   });
+  //   return () => {
+  //     window.removeEventListener('resize', () => {});
+  //   };
+  // }, []);
   // 监听echart容器和option变化
   useEffect(() => {
     init();
   }, [echartRef, props.option]);
+
   // 对父组件暴露获取ECharts实例的方法，可直接通过实例调用原生函数
   useImperativeHandle(ref, () => ({ getInstance }));
   return <div ref={echartRef} style={{ width: '100%', height: '100%' }}></div>;
