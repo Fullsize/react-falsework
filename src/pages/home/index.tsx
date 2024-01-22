368e380515f7777767b4f20199fd32d905c09bf0
无涯 <1619686166@qq.com>
2024-01-22 18:10:08 +0800

    feat:  更新

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from './index.module.css';
 import { useNavigate } from 'react-router-dom';
 import Outlet from '@/pages/outlet';
+import { accurate } from '@/utils';
 // import WithAuth from '@/hocs/withAuth';
 // import bg from '@images/1.png';
 import './index.css';
 const Page = () => {
   const navigate = useNavigate();
+  useEffect(() => {
+    console.log(accurate(5.01, 2));
+  }, []);
   return (
     <div className={styles['constainer']}>
       <div className="name">home</div>
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,11 @@
 import { floor } from 'lodash';
 import qs from 'qs';
-// px to vw = 100vw/设计图宽度*指定px
+import Decimal from 'decimal.js';
+/**
+ * @description: px to vw = 100vw/设计图宽度*指定px
+ * @param {number} px 宽度px
+ * @return {*}
+ */
 export const px2vw = (px: number) => `${floor((100 / 1920) * px)}vw`;
 export const deconstruction = (
   data: any[],
@@ -25,7 +30,13 @@ export const deconstruction = (
   });
 };
 
-// 修改 url 里面的指定参数
+/**
+ * @description: 修改地址栏参数
+ * @param {string} name key
+ * @param {any} val 数值
+ * @param {any} href 地址
+ * @return {*}
+ */
 export function changeURLArg(name?: string, val?: any, href?: any) {
   const url = href || window.location.href;
   const pattern = name + '=([^&]*)';
@@ -44,7 +55,11 @@ export function changeURLArg(name?: string, val?: any, href?: any) {
   return `${url}?${replaceText}`;
   return url + '\n' + name + '\n' + val;
 }
-
+/**
+ * @description: 获取query参数
+ * @param {string} search 地址栏参数
+ * @return {*}
+ */
 export const getQueryString = (search?: string) => {
   search =
     search ||
@@ -52,7 +67,12 @@ export const getQueryString = (search?: string) => {
     window.location.hash.split('?')[1];
   return qs.parse(search);
 };
-
+/**
+ * @description: 获取物理分辨率
+ * @param {*} containRatio 是否获取
+ * @param {*} baseWidth 物理分辨率(px)
+ * @return {*}
+ */
 export const getScale = (containRatio = true, baseWidth = 1920) => {
   const currentScale = document.documentElement.clientWidth / baseWidth;
   const formattedScale = currentScale > 1 ? currentScale : 1;
@@ -60,4 +80,15 @@ export const getScale = (containRatio = true, baseWidth = 1920) => {
     ? formattedScale * window.devicePixelRatio
     : formattedScale;
   return Math.ceil(resultScale);
+};
+/**
+ * @description: 精度转换方法
+ * @param {number} num 需要转换的数值
+ * @param {*} precision 保留精度位数
+ * @return {*}
+ */
+export const accurate = (num: number, precision = 2) => {
+  const decimalNum = new Decimal(num);
+  const strippedNum = decimalNum.toDecimalPlaces(precision);
+  return +strippedNum.toString();
 };
