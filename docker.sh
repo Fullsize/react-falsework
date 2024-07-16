c255f9fff5702141e8a208a3d7910e2c11326508
无涯 <1619686166@qq.com>
2024-07-16 15:34:07 +0800

    feat: 更新脚本

--- docker.sh
+++ docker.sh
@@ -1,4 +1,4 @@
 DOCKER_BUILDKIT=1 docker build . -t react_dist  --progress=plain
-docker stop react_dist
-docker rm react_dist
+docker stop react_dist｜true
+docker rm react_dist｜true
 docker run -it -d -p 8093:80 --name react_dist  react_dist
